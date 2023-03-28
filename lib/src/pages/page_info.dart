eb8e8602036f54f8fc3762162a6c80e31de1d45a
JulioAGuevara <julio.guevara@unah.hn>
2023-03-27 19:42:16 -0600

    mas mejoras

--- lib/src/pages/page_info.dart
+++ lib/src/pages/page_info.dart
@@ -41,7 +41,7 @@ class InfoPage extends StatelessWidget {
                   ],
                 ),
 
-                const SizedBox(height: 20),
+                const SizedBox(height: 10),
 
                 //Opciones Contacto
                 Padding(
@@ -51,70 +51,10 @@ class InfoPage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       
-                      Container(
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                          boxShadow: const [BoxShadow(
-                            color: Color.fromARGB(188, 139, 195, 74),
-                            blurRadius: 20, 
-                            offset: Offset(0, 10))],
-                          color: Colors.lightGreen,
-                          borderRadius: BorderRadius.circular(50)
-                        ),
-                        alignment: Alignment.center,
-                        child: Container(
-                          height: 45,
-                          width: 45,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(45)
-                          ),
-                          alignment: Alignment.center,
-                          child: Container(
-                            height: 40,
-                            width: 40,
-                            decoration: BoxDecoration(
-                              color: Colors.lightGreen,
-                              borderRadius: BorderRadius.circular(40)
-                            ),
-                            child: IconButton(onPressed: (){},icon: const Icon(Icons.call_rounded, color: Colors.white,),)
-                            ),
-                        ),
-                      ),
+                      TopBtns(color: Colors.lightGreen, icon: Icons.call_rounded, shadow: Colors.lightGreenAccent[100]),
 
-                      Container(
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                          color: Colors.yellow,
-                          borderRadius: BorderRadius.circular(50),
-                          boxShadow: const [BoxShadow(
-                            color: Color.fromARGB(185, 255, 235, 59),
-                            blurRadius: 20, 
-                            offset: Offset(0, 10))],
-                        ),
-                        alignment: Alignment.center,
-                        child: Container(
-                          height: 45,
-                          width: 45,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(45)
-                          ),
-                          alignment: Alignment.center,
-                          child: Container(
-                            height: 40,
-                            width: 40,
-                            decoration: BoxDecoration(
-                              color: Colors.yellow,
-                              borderRadius: BorderRadius.circular(40)
-                            ),
-                            child: IconButton(onPressed: (){},icon: const Icon(Icons.message, color: Colors.white,),)
-                            ),
-                        ),
-                      ),
-                      
+                      TopBtns(color: Colors.yellow, icon: Icons.message, shadow: Colors.yellowAccent[100]),
+
                       Container(
                         height: 80,
                         width: 80,
@@ -141,8 +81,8 @@ class InfoPage extends StatelessWidget {
                             ),
                             child: const CircleAvatar(
                                 minRadius: double.infinity,
-                                backgroundImage: NetworkImage(
-                                  'https://cdn.hswstatic.com/gif/play/0b7f4e9b-f59c-4024-9f06-b3dc12850ab7-1920-1080.jpg',
+                                backgroundImage: AssetImage(
+                                  'images/profile.jpg',
                                   ),
                                 
                             ),
@@ -150,69 +90,14 @@ class InfoPage extends StatelessWidget {
                         ),
                       ),
 
-                      Container(
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(50),
-                          boxShadow: const [BoxShadow(
-                            color: Color.fromARGB(158, 244, 67, 54),
-                            blurRadius: 20, 
-                            offset: Offset(0, 10))],
-                        ),
-                        alignment: Alignment.center,
-                        child: Container(
-                          height: 45,
-                          width: 45,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(45)
-                          ),
-                          alignment: Alignment.center,
-                          child: Container(
-                            height: 40,
-                            width: 40,
-                            decoration: BoxDecoration(
-                              color: Colors.red,
-                              borderRadius: BorderRadius.circular(40)
-                            ),
-                            child: IconButton(onPressed: (){},icon: const Icon(Icons.share, color: Colors.white,),)
-                            ),
-                        ),
-                      ),
 
-                      Container(
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(50),
-                          boxShadow: const [BoxShadow(
-                            color: Color.fromARGB(148, 33, 149, 243),
-                            blurRadius: 20, 
-                            offset: Offset(0, 10))],
-                        ),
-                        alignment: Alignment.center,
-                        child: Container(
-                          height: 45,
-                          width: 45,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(45)
-                          ),
-                          alignment: Alignment.center,
-                          child: Container(
-                            height: 40,
-                            width: 40,
-                            decoration: BoxDecoration(
-                              color: Colors.blue,
-                              borderRadius: BorderRadius.circular(40)
-                            ),
-                            child: IconButton(onPressed: (){},icon: const Icon(Icons.star_outline_outlined, color: Colors.white,),)
-                            ),
-                        ),
-                      ),
+                      TopBtns(color: Colors.red, icon: Icons.share, shadow: Colors.redAccent[100]),
+
+                      TopBtns(color: Colors.blue, icon: Icons.star_outline_outlined, shadow: Colors.blueAccent[100]),
+
+                      // 
+
+                      
 
                       
                     ],
@@ -281,7 +166,7 @@ class InfoPage extends StatelessWidget {
                   ),
                 ),
 
-                const SizedBox(height: 10,),
+                const SizedBox(height: 15,),
 
                 //Contact info Container Primary
                 Cont1btn(
@@ -389,15 +274,15 @@ class InfoPage extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         //btn Facebook
-                        BtnWImg(col: Colors.blue[900],url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/640px-Facebook_Logo_%282019%29.png'),
+                        BtnWImg(col: Colors.blue[900],url: 'images/facebook.png'),
                         //btn Twitter
-                        BtnWImg(col: Colors.blue[300],url: 'https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-twitter-social-media-round-icon-png-image_6315985.png'),
+                        BtnWImg(col: Colors.blue[300],url: 'images/twitter.png'),
                         //btn Instagram
-                        BtnWImg(col: Colors.pink[300],url: 'https://www.citypng.com/public/uploads/preview/-51608566170ihbmnwcovp.png'),
+                        BtnWImg(col: Colors.pink[300],url: 'images/Instagram.png'),
                         //btn Google +
-                        BtnWImg(col: Colors.orange[900],url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Google_Plus_logo_%282015-2019%29.svg/480px-Google_Plus_logo_%282015-2019%29.svg.png'),
+                        BtnWImg(col: Colors.orange[900],url: 'images/G+.png'),
                         //btn Mas opciones
-                        BtnWImg(col: Colors.grey[900],url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9acrIBvICqIJFbikyOOCLSBudtqNWtA68Cw&usqp=CAU'),
+                        BtnWImg(col: Colors.grey[900],url: 'images/more.png'),
                       ],
                     ),
                   ),)
@@ -497,7 +382,7 @@ class BtnWImg extends StatelessWidget {
             ),
             child: CircleAvatar(
               minRadius: double.infinity,
-              backgroundImage: NetworkImage(
+              backgroundImage: AssetImage(
                 '$url',
               ), 
             ),      
@@ -506,5 +391,52 @@ class BtnWImg extends StatelessWidget {
       ),
     );
                       
+  }
+}
+
+class TopBtns extends StatelessWidget {
+  const TopBtns({super.key, required this.color, required this.icon, required this.shadow});
+
+  final color;
+  final icon;
+  final shadow;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+        boxShadow: [BoxShadow(
+          color: shadow,
+          blurRadius: 20, 
+          offset: const Offset(0, 10)
+        )],
+        color: color,
+        borderRadius: BorderRadius.circular(50)
+      ),
+      alignment: Alignment.center,
+      child: Container(
+        height: 45,
+        width: 45,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(45)
+        ),
+        alignment: Alignment.center,
+        child: Container(
+          height: 40,
+          width: 40,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(40)
+          ),
+          child: IconButton(
+            onPressed: (){},
+            icon: Icon(icon, color: Colors.white,),
+            )
+          ),
+        ),
+      );
   }
 }
